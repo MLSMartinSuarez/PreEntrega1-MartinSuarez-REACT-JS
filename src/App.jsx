cc20c2d924733e78e94cd5c0b9fc6675e2f80e3e
Martin Suarez <martinleonel.94@gmail.com>
2024-01-11 22:40:19 -0300

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './index.css'
 const App = () => {
   return(
     <div>
-      <ItemListContainer mensaje={"Bienvenido"} /> 
+      <ItemListContainer mensaje={"Bienvenido Usuario"} /> 
       <Header />
       <Banner />
       <ItemListContainer mensaje={"SITIO EN PROCESO"} /> 
