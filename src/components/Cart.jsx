baab03da1d578860d4584992a2c604e681e9401e
MLSMartinSuarez <martinleonel.94@gmail.com>
2024-02-25 21:12:29 -0300

    proyecto final avanzado

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,11 @@
 const Cart = () => {
     return (
         <>
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, est iure laborum minus repellat accusantium veniam cupiditate alias molestias nisi libero doloribus ipsum hic architecto laudantium tenetur ut! Dicta, molestiae?
+            </p>
+        </div>
         </>
     )
 }
