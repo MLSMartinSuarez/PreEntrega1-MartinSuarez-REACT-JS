a74217e2dd10f2ff598824b72ba25723c25c8867
MLSMartinSuarez <martinleonel.94@gmail.com>
2024-03-12 20:16:21 -0300

    detalles

--- src/components/ContactoHeader.jsx
+++ src/components/ContactoHeader.jsx
@@ -1,8 +1,23 @@
 const ContactoHeader = () => {
     return(
+        <>
         <div className="ContactoHeader">
-            <p>Hablanos de lunes a viernes de 8.30 a 17.30hs - WhatsApp 1168873346</p>
+            <div className="socials">
+                <div>
+                    <a href="https://web.whatsapp.com/"><img className="socialsLogo" src="https://brandlogos.net/wp-content/uploads/2022/05/instagram-2022-logo_brandlogos.net_kamyn.png" alt="instagram logo" /></a>
+                    <p>blancocarmiel_official</p>
+                </div>
+                <div>
+                    <a href="https://www.instagram.com/reel/C3l-y83OHWi/?igsh=M2owdnN2Y240dzd1"><img className="socialsLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2044px-WhatsApp.svg.png" alt="whatsapp logo" /></a>
+                    <p>8:30 a 17:30 - consultanos</p>
+                </div>
+                <div>
+                   <a href="https://twitter.com/?lang=es"> <img className="socialsLogo" src="https://cdn.icon-icons.com/icons2/4029/PNG/512/twitter_x_new_logo_x_rounded_icon_256078.png" alt="x logo" /></a>
+                    <p>blancocarmiel_official</p>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,20 @@
+import ContactoHeader from "./ContactoHeader";
 import ItemCount from "./ItemCount";
 
 const Footer = () => {
     return (
 
+        
         <div className="footer ">
-            <p>©Copyright 2024. Todos los Derechos Reservados.</p> 
-            <p>Centro de Atención al Cliente: 0800-362-2740 / L a V de 8:30 a 17:30 hs</p>
+            <div className="bg">
+                <p>©Copyright 2024. Todos los Derechos Reservados.</p> 
+                <p>Centro de Atención al Cliente: 0800-362-2740 / L a V de 8:30 a 17:30 hs</p>
+            </div>
+            <div>
+                <ContactoHeader />
+            </div>
         </div>
-
+        
 
     )
 }
